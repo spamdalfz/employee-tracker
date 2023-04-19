3ccf69e3621300b9d53bf5a603781e198a541738
spamdalfz <coreyvasser@gmail.com>
2023-04-18 21:58:54 -0700

    added inital prompts

--- src/db.js
+++ src/db.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
+// Configure the database connection
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -8,4 +9,14 @@ const connection = mysql.createConnection({
     database: 'employee_db',
 });
 
+// Connect to the database
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the MySQL database');
+});
+
+// Export the connection object for use in other modules
 module.exports = connection;
--- src/prompt.js
+++ src/prompt.js
@@ -1,13 +1,124 @@
-employee - tracker /
-    db /
-    schema.sql
-seeds.sql
-│
-├── src /
-│   ├── db.js
-│   ├── server.js
-│   ├── prompts.js
-│   ├── schema.sql
-│   
-│  
-└── index.js
+const inquirer = require('inquirer');
+const {
+    getDepartments,
+    getRoles,
+    getEmployees,
+    addDepartment,
+    addRole,
+    addEmployee,
+    updateEmployeeRole
+} = require('./queries');
+
+// Prompt for main menu actions
+const mainPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',
+                'Exit'
+            ]
+        }
+    ]);
+};
+
+// Prompt for adding a new department
+const addDepartmentPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'departmentName',
+            message: 'What is the name of the department?'
+        }
+    ]);
+};
+
+// Prompt for adding a new role
+const addRolePrompt = async () => {
+    const departmentChoices = await getDepartments();
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'roleTitle',
+            message: 'What is the title of the role?'
+        },
+        {
+            type: 'input',
+            name: 'roleSalary',
+            message: 'What is the salary for this role?'
+        },
+        {
+            type: 'list',
+            name: 'roleDepartment',
+            message: 'Which department does this role belong to?',
+            choices: departmentChoices
+        }
+    ]);
+};
+
+// Prompt for adding a new employee
+const addEmployeePrompt = async () => {
+    const roleChoices = await getRoles();
+    const managerChoices = await getEmployees();
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'employeeFirstName',
+            message: "What is the employee's first name?"
+        },
+        {
+            type: 'input',
+            name: 'employeeLastName',
+            message: "What is the employee's last name?"
+        },
+        {
+            type: 'list',
+            name: 'employeeRole',
+            message: 'What is the role of the employee?',
+            choices: roleChoices
+        },
+        {
+            type: 'list',
+            name: 'employeeManager',
+            message: 'Who is the manager of the employee?',
+            choices: [...managerChoices, { name: 'None', value: null }]
+        }
+    ]);
+};
+
+// Prompt for updating an employee role
+const updateEmployeeRolePrompt = async () => {
+    const employeeChoices = await getEmployees();
+    const roleChoices = await getRoles();
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'employeeToUpdate',
+            message: 'Which employee do you want to update?',
+            choices: employeeChoices
+        },
+        {
+            type: 'list',
+            name: 'newRole',
+            message: 'What is the new role for this employee?',
+            choices: roleChoices
+        }
+    ]);
+};
+
+// Export the prompt functions
+module.exports = {
+    mainPrompt,
+    addDepartmentPrompt,
+    addRolePrompt,
+    addEmployeePrompt,
+    updateEmployeeRolePrompt
+};
